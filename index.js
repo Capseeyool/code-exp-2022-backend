87da754f7daf2815482573bb23bb474f6384c169
Capseeyool <capsulechew@gmail.com>
2022-06-15 10:10:40 +0800

    .

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ app.get('/events', async (req, res) => {
         if (req.query.password === password.rows[0].password) {
             const events = await client.query('SELECT * FROM events WHERE user_username = $1', [req.query.username])
             console.log(events.rows)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send(events.rows)
         } else {
             res.sendStatus(401)
